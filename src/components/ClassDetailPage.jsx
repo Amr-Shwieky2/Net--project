5f38d8be9099dab717700462e5900b1e8f42ed75
Amr Shwieky <131922418+Amr-Shwieky2@users.noreply.github.com>
2024-09-28 04:45:20 +0300

    want to import to netlify6

--- src/components/ClassDetailPage.jsx
+++ src/components/ClassDetailPage.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
